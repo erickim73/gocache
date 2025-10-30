1bd9c04d3b51374fc833172906207740862fdd6e
erickim73 <seyoon2006@gmail.com>
2025-10-29 21:26:17 -0500

    added thread safety

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,8 +1,11 @@
 package cache
 
+import "sync"
+
 type Cache struct {
 	data map[string]string
 	maxSize int
+	mu sync.RWMutex   // read write lock
 }
 
 func New(maxSize int) *Cache {
@@ -13,14 +16,23 @@ func New(maxSize int) *Cache {
 }
 
 func (c *Cache) Set(key, value string) {
+	c.mu.Lock() 		// exclusive access for writes
+	defer c.mu.Unlock()
+
 	c.data[key] = value
 }
 
 func (c *Cache) Get(key string) (string, bool) {
+	c.mu.RLock() 		// multiple readers can enter
+	defer c.mu.RUnlock()
+	
 	value, exists := c.data[key]
 	return value, exists
 }
 
 func (c *Cache) Delete(key string) {
+	c.mu.Lock() 		// exclusive access for writes
+	defer c.mu.Unlock()
+	
 	delete(c.data, key)	
 }
