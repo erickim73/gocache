825d14189832d8ea6f609c3f5c8a92087f9c8a59
erickim73 <seyoon2006@gmail.com>
2025-11-06 20:23:03 -0600

    added bulk encoder

--- pkg/protocol/encoder.go
+++ pkg/protocol/encoder.go
@@ -1,5 +1,16 @@
 package protocol
 
-func EncodeString(s string) string {
+import (
+	"strconv"
+)
+
+// Encodes a simple string s to '+s\r\n'
+func EncodeSimpleString(s string) string {
 	return "+" + s + "\r\n"
+}
+
+// Encodes a Bulk String to #[length]\r\n[string]\r\n
+func EncodeBulkString(s string) string {
+	var length string = strconv.Itoa(len(s))
+	return "$" + length + "\r\n" + s + "\r\n"
 }
--- pkg/protocol/protocol_test.go
+++ pkg/protocol/protocol_test.go
@@ -4,11 +4,20 @@ import (
 	"testing"
 )
 
-func TestEncodeString(t *testing.T) {
-	result := EncodeString("OK")
+func TestEncodeSimpleString(t *testing.T) {
+	result := EncodeSimpleString("OK")
 	expected := "+OK\r\n"
 
 	if result != expected {
-		t.Errorf("Expected to get %s, got %s", expected, result)
+		t.Errorf("Encoding simple string got %s, expected %s", result, expected)
+	}
+}
+
+func TestEncodeBulkString(t *testing.T) {
+	result := EncodeBulkString("Hello World")
+	expected := "$11\r\nHello World\r\n"
+
+	if result != expected {
+		t.Errorf("Encoding bulk string got %s, expected %s", result, expected)
 	}
 }
