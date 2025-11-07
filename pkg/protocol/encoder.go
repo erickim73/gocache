96890639900f105839a005785e96a20b76efad84
erickim73 <seyoon2006@gmail.com>
2025-11-06 21:23:19 -0600

    Added EncodeArray

--- pkg/protocol/encoder.go
+++ pkg/protocol/encoder.go
@@ -4,12 +4,12 @@ import (
 	"strconv"
 )
 
-// Encodes a simple string s to '+s\r\n'
+// Encodes a simple string s to "+s\r\n"
 func EncodeSimpleString(s string) string {
 	return "+" + s + "\r\n"
 }
 
-// Encodes a Bulk String to #[length]\r\n[string]\r\n
+// Encodes a Bulk String to "#[length]\r\n[string]\r\n"
 func EncodeBulkString(s string, isNull bool) string {
 	if isNull {
 		return "$-1\r\n"
@@ -19,13 +19,23 @@ func EncodeBulkString(s string, isNull bool) string {
 	return "$" + length + "\r\n" + s + "\r\n"
 }
 
-// Encodes a int to :[num]\r\n
+// Encodes a int to ":[num]\r\n"
 func EncodeInteger(n int) string {
 	return ":" + strconv.Itoa(n) + "\r\n"
 }
 
-// Encodes an error message into -[Error]\r\n
+// Encodes an error message into "-[Error]\r\n"
 func EncodeError(s string) string {
 	return "-" + s + "\r\n"
 }
 
+// Encodes an array into "*[num of elements]\r\n[element1]"
+func EncodeArray(elements []string) string {
+	result := "*" + strconv.Itoa(len(elements)) + "\r\n"
+	
+	for _, value := range elements {
+		result += EncodeBulkString(value, false)
+	}
+	
+	return result
+}
