005cf44fc07f5553ae4d117757e7700b298caf11
erickim73 <seyoon2006@gmail.com>
2025-11-10 17:41:01 -0600

    test with set and get commands

--- pkg/test/test_client.go
+++ pkg/test/test_client.go
@@ -13,10 +13,17 @@ func main() {  // Changed from test_client
 	}
 	defer conn.Close()
 
-	resp := "*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n"
+	resp := "*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$7\r\ntesting\r\n"
 	conn.Write([]byte(resp))
 
 	buf := make([]byte, 1024)
 	n, _ := conn.Read(buf)
 	fmt.Println("Server response: ", string(buf[:n]))
+
+	resp2 := "*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n"
+	conn.Write([]byte(resp2))
+
+	buf2 := make([]byte, 1024)
+	n, _ = conn.Read(buf2)
+	fmt.Println("value for key: ", string(buf2[:n]))
 }
