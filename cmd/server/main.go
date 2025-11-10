71c404e449653c841ceb23dc7ddff67d0a667866
erickim73 <seyoon2006@gmail.com>
2025-11-09 18:44:13 -0600

    simple example of tcp server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	"net"
+	"log"
 	"fmt"
 )
 
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	// read from the client
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println(string(buf[:n]))
+
+	fmt.Fprintf(conn, "Echo = " + string(buf[:n]))
+}
+
 func main() {
-	fmt.Println("Hello world")
+	ln, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer ln.Close()
+
+	for {
+		// accept an incoming connection
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+
+		// handle the connection
+		go handleConnection(conn)
+	}
 }
