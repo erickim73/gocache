58be450859d05da1925f741076fb186966408e3b
erickim73 <seyoon2006@gmail.com>
2025-11-10 18:56:53 -0600

    test cache tcp connection

--- pkg/test/test_client.go
+++ pkg/test/test_client.go
@@ -13,6 +13,7 @@ func main() {  // Changed from test_client
 	}
 	defer conn.Close()
 
+	// SET key: testing
 	resp := "*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$7\r\ntesting\r\n"
 	conn.Write([]byte(resp))
 
@@ -20,10 +21,36 @@ func main() {  // Changed from test_client
 	n, _ := conn.Read(buf)
 	fmt.Println("Server response: ", string(buf[:n]))
 
+	// GET key
 	resp2 := "*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n"
 	conn.Write([]byte(resp2))
 
 	buf2 := make([]byte, 1024)
 	n, _ = conn.Read(buf2)
 	fmt.Println("value for key: ", string(buf2[:n]))
+
+	// DEL key
+	resp3 := "*2\r\n$3\r\nDEL\r\n$3\r\nkey\r\n"
+	conn.Write([]byte(resp3))
+
+	buf3 := make([]byte, 1024)
+	n, _ = conn.Read(buf3)
+	fmt.Println("Deleted response: ", string(buf[:n]))
+
+	// GET key
+	resp4 := "*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n"
+	conn.Write([]byte(resp4))
+
+	buf4 := make([]byte, 1024)
+	n, _ = conn.Read(buf4)
+	fmt.Println("value for key: ", string(buf4[:n]))
+
+	// FDS key
+	resp5 := "*2\r\n$3\r\nFDS\r\n$3\r\nkey\r\n"
+	conn.Write([]byte(resp5))
+
+	buf5 := make([]byte, 1024)
+	n, _ = conn.Read(buf5)
+	fmt.Println("FDS response: ", string(buf5[:n]))
 }
+
