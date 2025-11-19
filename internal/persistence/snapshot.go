8fb07c7d5f757b58d093d4929eb4a5247b116942
erickim73 <seyoon2006@gmail.com>
2025-11-19 11:25:53 -0600

    iterate over snapshot and write to tempFIle

--- internal/persistence/snapshot.go
+++ internal/persistence/snapshot.go
@@ -1,17 +1,21 @@
 package persistence
 
 import (
+	"strconv"
 	"time"
+	"fmt"
+
+	"github.com/erickim73/gocache/pkg/protocol"
 )
 
 
 
-func (aof *AOF) rewriteAOF () (*AOF, error) {
+func (aof *AOF) rewriteAOF () (error) {
 	// create a new temp aof file
 	tempName := aof.fileName + "_temp"
 	tempFile, err := NewAOF(tempName, aof.policy, aof.cache)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	// create a snapshot of the cache
@@ -25,7 +29,13 @@ func (aof *AOF) rewriteAOF () (*AOF, error) {
 				continue // skip expired items
 			}
 		}
-
+		ttl := entry.ExpiresAt.Sub(time.Now())
+		ttlSeconds := strconv.Itoa(int(ttl.Seconds()))
+		aofCommand := protocol.EncodeArray([]string{"SET", key, entry.Value, ttlSeconds})
+		err := tempFile.Append(aofCommand)
+		if err != nil {
+			fmt.Printf("Failed to write to temp AOF file: %v\n", err)
+		}
 	}
 
 
