3c065b817ece0b8fbf793a463c4206f15b6597f0
erickim73 <seyoon2006@gmail.com>
2025-11-23 15:28:34 -0600

    add yamlConfig

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,8 +3,18 @@ package config
 import (
 	"time"
 
-	"gopkg.in/yaml.v3"
+	// "gopkg.in/yaml.v3"
 )
+
+type yamlConfig struct {
+	Port int `yaml:"port"`
+	MaxCacheSize int `yaml:"max_cache_size"`
+	AOFFIleName string `yaml:"aof_file"`
+	SnapshotFileName string `yaml:"snapshot_file"`
+	SyncPolicy string `yaml:"sync_policy"`
+	SnapshotIntervalSeconds int `yaml:"snapshot_interval_seconds"`
+	GrowthFactor int64 `yaml:"growth_factor"`
+}
 
 type Config struct {
 	// server settings
