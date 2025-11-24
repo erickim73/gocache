980c2c3094b73b40b9f75608525b995a31cc76cf
erickim73 <seyoon2006@gmail.com>
2025-11-23 19:25:14 -0600

    cleanup benchmark helper function

--- internal/persistence/aof_benchmark_test.go
+++ internal/persistence/aof_benchmark_test.go
@@ -2,6 +2,7 @@ package persistence
 
 import (
 	"fmt"
+	"os"
 	"testing"
 	"time"
 
@@ -13,14 +14,22 @@ func setupBenchmark(b *testing.B, policy SyncPolicy) (*cache.Cache, *AOF) {
 	c, _ := cache.New(100000)
 
 	// use unique filename for each test
-	filename := fmt.Sprintf("bench_test_%d.aof", time.Now().UnixNano())
+	fileName := fmt.Sprintf("bench_test_%d.aof", time.Now().UnixNano())
 	snapshotName := fmt.Sprintf("bench_test_%d.rdb", time.Now().UnixNano())
 
-	aof, err := NewAOF(filename, snapshotName, policy, c, 100)
+	aof, err := NewAOF(fileName, snapshotName, policy, c, 100)
 	if err != nil {
 		b.Fatalf("Failed to create AOF: %v", err)
 	}
 
 	return c, aof
+}
+
+func cleanupBenchmark(aof *AOF) {
+	fileName := aof.fileName
+	snapshotName := aof.snapshotName
+	aof.Close()
+	os.Remove(fileName)
+	os.Remove(snapshotName)
 }
 
