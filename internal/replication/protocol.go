1b1a433efdc2ce0d1c89a8bb110ec48f21b91102
erickim73 <seyoon2006@gmail.com>
2025-11-25 18:19:51 -0600

    structure for replicate command

--- internal/replication/protocol.go
+++ internal/replication/protocol.go
@@ -1,6 +1,19 @@
 package replication
 
+import (
+	"time"
+)
+
 type SyncRequest struct {
 	FollowerID string //unique id of follower
 	LastSeqNum int64 // last sequence number received (for reconnection)
 }
+
+type ReplicateCommand struct {
+	SeqNum int64
+	Operation string
+	Key string
+	Value string
+	TTL time.Time
+}
+
