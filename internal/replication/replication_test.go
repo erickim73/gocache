ce700f7b4896bbbe544b235e0aa6e168b67c66c0
erickim73 <seyoon2006@gmail.com>
2025-11-26 01:11:40 -0600

    test delete command

--- internal/replication/replication_test.go
+++ internal/replication/replication_test.go
@@ -16,7 +16,7 @@ func TestReplicateCommandRoundTrip(t *testing.T) {
 	encoded := EncodeReplicateCommand(SetCmd)
 	decoded, err := DecodeReplicateCommand(encoded)
 	if err != nil {
-		t.Fatalf("Error Decodeing Replicate Command: %v", err)
+		t.Fatalf("Error Decoding Replicate Command: %v", err)
 	}
 
 	var decodedCmd *ReplicateCommand
@@ -39,5 +39,40 @@ func TestReplicateCommandRoundTrip(t *testing.T) {
 	}
 	if decodedCmd.Value != "value" {
 		t.Errorf("Value should be value, got %s", decodedCmd.Value)
+	}
+
+	// test delete
+	delCmd := &ReplicateCommand{
+		SeqNum: 43,
+		Operation: OpDelete,
+		Key: "testing",
+	}
+
+	encoded2 := EncodeReplicateCommand(delCmd)
+	decoded2, err := DecodeReplicateCommand(encoded2)
+	if err != nil {
+		t.Fatalf("Error Decoding Replicate Command: %v", err)
+	}
+
+	var decodedCmd2 *ReplicateCommand
+	if dc, ok := decoded2.(*ReplicateCommand); ok {
+		decodedCmd2 = dc
+	} else if dcVal, ok := decoded2.(ReplicateCommand); ok {
+		decodedCmd2 = &dcVal
+	} else {
+		t.Fatalf("Decoded command is not a ReplicateCommand, got %T", decoded)
+	}
+
+	if decodedCmd2.SeqNum != 43 {
+		t.Errorf("SeqNum should be 43, got %d", decodedCmd2.SeqNum)
+	}
+	if decodedCmd2.Operation != OpDelete {
+		t.Errorf("Operation should be DELETE, got %s", decodedCmd2.Operation)
+	}
+	if decodedCmd2.Key != "testing" {
+		t.Errorf("Key should be 'testing', got %s", decodedCmd2.Key)
+	}
+	if decodedCmd2.Value != "" {
+		t.Errorf("Delete command Value should be empty, got %s", decodedCmd2.Value)
 	}
 }
