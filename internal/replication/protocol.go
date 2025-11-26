0306d6dcf322ee3fe7927ce9554f92477582d60c
erickim73 <seyoon2006@gmail.com>
2025-11-25 23:38:13 -0600

    DecodeeplicateCommand function

--- internal/replication/protocol.go
+++ internal/replication/protocol.go
@@ -109,3 +109,79 @@ func EncodeReplicateCommand(comm *ReplicateCommand) []byte {
 
 	return []byte(command)
 }
+
+func DecodeReplicateCommand(data []byte) (interface{}, error) {
+	reader := bufio.NewReader(bytes.NewReader(data))
+
+	parsed, err := protocol.Parse(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	arr, ok := parsed.([]interface{})
+	if !ok {
+		return nil, errors.New("expected array")
+	}
+
+	if len(arr) != 5 || len(arr) != 6 {
+		return nil, fmt.Errorf("expected length of array to be 6, got %d", len(arr))
+	}
+
+	cmd, ok := arr[0].(string)
+	if !ok || cmd != CmdReplicate {
+		return nil, errors.New("expected REPLICATE command")
+	}
+
+	// handle both string and int types for seqNum
+	var seqNum int64
+	switch v := arr[1].(type) {
+	case int:
+		seqNum = arr[1].(int64)
+	case string:
+		seqNum, err = strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("invalid sequence number: %w", err)
+		}
+	default:
+		return nil, fmt.Errorf("sequence number must be int or string, got %s", v)
+	}
+
+	operation, ok := arr[2].(string)
+	if !ok || operation != OpSet || operation != OpDelete {
+		return nil, errors.New("expected operation to be SET or DELETE")
+	}
+
+	key, ok := arr[3].(string)
+	if !ok {
+		return nil, errors.New("expected key to be a string")
+	}
+
+	var value string
+	var ttl int64
+	if operation == OpSet {
+		value, ok = arr[4].(string)
+		if !ok {
+			return nil, errors.New("expected value to be a string")
+		}
+
+		ttl, ok = arr[5].(int64)
+		if !ok {
+			return nil, errors.New("expected ttl to be of type int64")
+		}
+	} else if operation == OpDelete {
+		ttl, ok = arr[4].(int64)
+		if !ok {
+			return nil, errors.New("expected ttl to be of type int64")
+		}
+	}
+
+	return &ReplicateCommand{
+		SeqNum: seqNum,
+		Operation: operation,
+		Key: key,
+		Value: value,
+		TTL: ttl,
+	}, nil
+
+
+}
