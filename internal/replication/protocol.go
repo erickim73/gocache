2a4b40c2bde8a97b22d25ae67b70a1dc51abc94a
erickim73 <seyoon2006@gmail.com>
2025-11-25 19:37:20 -0600

    encode sync requests function

--- internal/replication/protocol.go
+++ internal/replication/protocol.go
@@ -2,6 +2,9 @@ package replication
 
 import (
 	"time"
+	"strconv"
+
+	"github.com/erickim73/gocache/pkg/protocol"
 )
 
 type SyncRequest struct {
@@ -20,4 +23,11 @@ type ReplicateCommand struct {
 type HeartBeat struct {
 	SeqNum int64
 	NodeID string
+}
+
+func EncodeSyncRequest(req *SyncRequest) ([]byte, error) {
+	lastSeqNum := strconv.FormatInt(req.LastSeqNum, 10)
+	command := protocol.EncodeArray([]string{"SYNC", req.FollowerID, lastSeqNum})
+
+	return []byte(command), nil
 }
