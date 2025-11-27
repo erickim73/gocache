f4619b2840bdbcda885e1f5aa2f0233de4284190
erickim73 <seyoon2006@gmail.com>
2025-11-26 22:12:53 -0600

    new leader function

--- internal/replication/leader.go
+++ internal/replication/leader.go
@@ -20,3 +20,12 @@ type Follower struct {
 	conn net.Conn // tcp connection to follower
 	id string // follower's id
 }
+
+func NewLeader(cache *cache.Cache, aof *persistence.AOF, port int) (*Leader, error) {
+	return &Leader{
+		cache: cache,
+		aof: aof,
+		followers: []*Follower{},
+		seqNum: 0,
+	}, nil
+}
