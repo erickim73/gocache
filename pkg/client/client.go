247ed85f2119b450d8f6ca99eb8aa109692c0ef2
erickim73 <seyoon2006@gmail.com>
2026-01-26 13:20:14 -0600

    Implemented NewClient() and Close() functions

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -20,6 +20,28 @@ type Client struct {
 	writer *bufio.Writer
 }
 
+// creates a new client connection
+func NewClient(address string) (*Client, error) {
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect: %v", err)
+	}
+
+	return &Client{
+		conn:   conn,
+		reader: bufio.NewReader(conn),
+		writer: bufio.NewWriter(conn),
+	}, nil
+}
+
+// close the client connection
+func (c *Client) Close() error {
+	if c.conn != nil {
+		return c.conn.Close()
+	}
+	return nil
+}
+
 func main() {
 	// create a tcp socket
 	conn, err := net.Dial("tcp", "localhost:6379")
