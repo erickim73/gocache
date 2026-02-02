dd1ef3c272e7038c484d07ca04d52836d3ca82c3
erickim73 <seyoon2006@gmail.com>
2026-02-02 11:05:19 -0600

    Add NodeHealth and HealthChecker structs

--- internal/cluster/health.go
+++ internal/cluster/health.go
@@ -1,7 +1,8 @@
 package cluster
 
 import (
-
+	"sync"
+	"time"
 )
 
 // represents the health state of a node
@@ -26,3 +27,32 @@ func (ns NodeStatus) String() string {
 	}
 }
 
+// tracks the health information for a single node
+type NodeHealth struct {
+	NodeID string
+	Address string
+	Status NodeStatus
+	LastSuccessfulPing time.Time
+	LastFailedPing time.Time
+	ConsecutiveFailures int
+	ConsecutiveSuccesses int
+}
+
+// monitors the health of all nodes in the cluster
+type HealthChecker struct {
+	nodeHealth map[string]*NodeHealth // nodeID -> health info
+
+	// config
+	checkInterval time.Duration // how often to check (5 sec)
+	failureThreshold int // failures before marking dead (3)
+	timeout time.Duration // ping timeout (2 sec)
+
+	// dependencies
+	hashRing *HashRing
+	onNodeFailed func(nodeID string) // callback when node dies
+	onNodeRecovered func(nodeID string) // callback when node recovers
+
+	mu sync.RWMutex
+	stopCh chan struct{}
+	wg sync.WaitGroup
+}
