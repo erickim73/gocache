0e9f5b40f4cc449adfa76ef3d2920ba53f277ee7
erickim73 <seyoon2006@gmail.com>
2026-02-11 21:00:29 -0600

    Add InitTextLogger()

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -47,5 +47,37 @@ func InitLogger(level LogLevel) {
 		"level", level,
 		"format", "json",
 	)
+}
 
+// creates human readable text logger
+func InitTextLogger(level LogLevel) {
+	var slogLevel slog.Level
+	switch level {
+	case LevelDebug:
+		slogLevel = slog.LevelDebug // most verbose, shows everything
+	case LevelInfo:
+		slogLevel = slog.LevelInfo // standard, shows info, warn, error
+	case LevelWarn:
+		slogLevel = slog.LevelWarn // only warnings and errors
+	case LevelError:
+		slogLevel = slog.LevelError // only errors
+	default:
+		slogLevel = slog.LevelInfo // default as info
+	}
+
+	// TextHandler produces output like:
+	// time=2024-02-10T10:00:00.000Z level=INFO msg="Server started" port=7000
+	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slogLevel, // minimum level to log
+		AddSource: false, // adds file:line information to logs
+	})
+
+	// set as default logger for entire application
+	slog.SetDefault(slog.New(handler))
+
+	// log that logging is initialized
+	slog.Info("Logger initialized",
+		"level", level,
+		"format", "json",
+	)
 }
